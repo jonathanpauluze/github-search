189edb0a94ecba10b72b6b6f9b36d14c379a5a75
Jonathan Pauluze <jonathanpauluze@gmail.com>
2019-09-17 14:23:40 -0300

    small fixes

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,7 @@ searchBtn.addEventListener('click', getInfo)
 
 async function getInfo(e) {
   if(e.keyCode === 13 || e.button === 0){
+    e.preventDefault()
     const searchQuery = searchInput.value
     if(searchQuery !== '') {
       response = await fetch(`https://api.github.com/users/${searchQuery}`)
@@ -18,10 +19,23 @@ async function getInfo(e) {
 }
 
 function setData(){
-  infoContainer.classList.add('border')
+  const date = new Date(response.created_at)
+  const opt = {day: 'numeric', month: 'long', year: 'numeric'}
+  console.log(date)
+  const memberSince = new Intl.DateTimeFormat('pt-BR', opt).format(date)
+
+  infoContainer.classList.add('card-border')
   infoContainer.innerHTML = `
     <img class="user-pic" src="${response.avatar_url}"/>
     <h2 class="user-name">${response.name}</h2>
     <p class="user-bio">${response.bio}</p>
+    <hr class="user-sep"/>
+    <p class="user-info"><i class="fas fa-map-marker-alt"></i> ${response.location}</p>
+    <p class="user-info"><i class="fas fa-calendar-alt"></i> ${memberSince}</p>
+    <a class="user-link" href="${response.html_url}" target="_blank">
+      <i class="fab fa-github"></i>
+      <span>Visit Profile</span>
+      <i class="fas fa-arrow-right"></i>
+    </a>
   `
 }
